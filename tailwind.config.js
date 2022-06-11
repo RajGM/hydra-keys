5ecad1e22bcaf79be0d2c0679215915b04074d08
Abdelhakeem <abdelhakeem.osama@hotmail.com>
2022-06-11 04:57:31 +0200

    Setup daisyUI
    Setup daisyUI and add skeleton for theme customization

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,5 +6,23 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        light: {
+          ...require("daisyui/src/colors/themes")["[data-theme=light]"],
+          primary: "#5F5AB4",
+          secondary: "#2F2E41",
+        },
+      },
+      {
+        dark: {
+          ...require("daisyui/src/colors/themes")["[data-theme=dark]"],
+          primary: "#5F5AB4",
+          secondary: "#5C5B69",
+        },
+      },
+    ],
+  },
 }
