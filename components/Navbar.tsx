d2f2c33d7d905213400a0991a437f6184bdc3029
Abdelhakeem <abdelhakeem.osama@hotmail.com>
2022-06-12 00:24:22 +0200

    Add empty 'create' page and link logo text to home

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -12,11 +12,15 @@ const Navbar = () => {
           height="90px"
           className="grayscale dark:grayscale-0 dark:brightness-0 dark:invert"
         />
-        <h1 className="text-xl font-bold">Hydra Wallet</h1>
+        <h1 className="text-xl font-bold">
+          <Link href="/">
+            Hydra Wallet
+          </Link>
+        </h1>
       </div>
       <div className="flex flex-row justify-end items-center gap-10">
         <Link href="/create">
-          <a>Create your wallet</a>
+          <a className="text-black">Create your wallet</a>
         </Link>
         <ThemeToggle />
         <button className="btn btn-secondary px-6 text-lg font-normal">
