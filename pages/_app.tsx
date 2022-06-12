80f323ec4deb4afd537382e9bee8b89f3db90efb
Abdelhakeem <abdelhakeem.osama@hotmail.com>
2022-06-12 07:47:48 +0200

    Set page container min-height

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,7 +7,7 @@ import { store } from '../redux/store/store'
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
-    <div className="container mx-auto">
+    <div className="container mx-auto min-h-screen">
       <Navbar />
       <Component {...pageProps} />
     </div>
