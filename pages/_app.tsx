db50e675e67095d657d851392a8d7d5f816ad27e
Abdelhakeem <abdelhakeem.osama@hotmail.com>
2022-06-12 07:47:48 +0200

    Set page container min-height

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,7 @@ import Navbar from '../components/Navbar'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <div className="container mx-auto">
+    <div className="container mx-auto min-h-screen">
       <Navbar />
       <Component {...pageProps} />
     </div>
