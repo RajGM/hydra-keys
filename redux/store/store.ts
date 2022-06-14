52de5203e83affd988e53c48469ca20d0d484c03
Abdelhakeem <abdelhakeem.osama@hotmail.com>
2022-06-14 13:45:21 +0200

    Add wallet reducer

--- redux/store/store.ts
+++ redux/store/store.ts
@@ -1,7 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
+import walletReducer from '../features/wallet/walletSlice'
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    wallet: walletReducer
+  },
 })
 
 export type AppDispatch = typeof store.dispatch
