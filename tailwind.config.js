93c4af7cfb5182963b382365776529e360c9b82f
Nikhil Sharma <nikhilsharmamusic2000@gmail.com>
2022-06-15 16:49:14 +0530

    added heading and para

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,23 +7,23 @@ module.exports = {
     extend: {},
   },
   darkMode: 'class',
-  plugins: [require("daisyui")],
+  plugins: [require('daisyui')],
   daisyui: {
     themes: [
       {
         light: {
-          ...require("daisyui/src/colors/themes")["[data-theme=light]"],
-          primary: "#2F2E41",
-          "primary-content": "#FFFFFF",
-          "--btn-text-case": "capitalize",
+          ...require('daisyui/src/colors/themes')['[data-theme=light]'],
+          primary: '#2F2E41',
+          'primary-content': '#FFFFFF',
+          '--btn-text-case': 'capitalize',
         },
       },
       {
         dark: {
-          ...require("daisyui/src/colors/themes")["[data-theme=dark]"],
-          primary: "#C27501",
-          "primary-content": "#F1F1F1",
-          "--btn-text-case": "capitalize",
+          ...require('daisyui/src/colors/themes')['[data-theme=dark]'],
+          primary: '#C27501',
+          'primary-content': '#F1F1F1',
+          '--btn-text-case': 'capitalize',
         },
       },
     ],
