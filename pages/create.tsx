972ffb9389fe04cc4ccee5c216c212f38acd9c64
Tess99854 <stesnime@yahoo.com>
2022-06-15 15:06:40 +0100

    fix page margin

--- pages/create.tsx
+++ pages/create.tsx
@@ -4,7 +4,7 @@ import CreateWalletForm from '../components/CreateWalletForm'
 
 const Create: NextPage = () => {
   return (
-    <div className="container mx-auto h-4/5 gap-10 flex flex-col justify-center items-center">
+    <div className="container mx-auto gap-10 flex flex-col justify-center items-center my-10">
       <h1 className="w-full text-center md:text-left text-3xl md:text-4xl font-bold text-primary dark:text-white">
         Create a Hydra Wallet
       </h1>
