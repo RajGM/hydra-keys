b8570430982a6423caa443d7c42f870e161c7e2d
Nikhil Sharma <nikhilsharmamusic2000@gmail.com>
2022-06-15 16:49:14 +0530

    added heading and para

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,16 @@ const Home: NextPage = () => {
 
       <main className="overflow-hidden py-5 ">
         <div className="flex w-full h-screen max-h-[85vh]">
-          <div className="w-1/2">02</div>
+          <div className="w-1/2 flex flex-col justify-center">
+            <h1 className="font-bold text-xl text-[#3F3D56] dark:text-[#E4E4E4]">
+              What is Hydra ?
+            </h1>
+            <p className="font-normal text-xl text-[rgb(0,0,0)] dark:text-[#F9F8F8]">
+              Hydra is a wallet of wallets. It enables extremely large
+              membership sets that can take part in fund distribution from a
+              central wallet. It works with SOL and any SPL token.
+            </p>
+          </div>
           <div className="w-1/2 flex justify-end items-center">
             <HomeBannerSvg
               className={styles.homeBannerSvg}
