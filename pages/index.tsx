0d90ffbc98cc5c7db1d6e931cfb8524317e54e40
Nikhil Sharma <nikhilsharmamusic2000@gmail.com>
2022-06-15 17:06:45 +0530

    added button arrows

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import HomeBannerSvg from '../assets/svg/homeBanner'
+import ArrowSvg from '../assets/svg/arrow'
 import styles from './../styles/Home.module.css'
 
 const Home: NextPage = () => {
@@ -14,7 +15,7 @@ const Home: NextPage = () => {
       <main className="overflow-hidden py-5 ">
         <div className="flex w-full h-screen max-h-[85vh]">
           <div className="w-1/2 flex flex-col justify-center">
-            <h1 className="font-bold text-xl text-[#3F3D56] dark:text-[#E4E4E4]">
+            <h1 className="font-bold text-2xl text-[#3F3D56] dark:text-[#E4E4E4]">
               What is Hydra ?
             </h1>
             <p className="font-normal text-xl text-[rgb(0,0,0)] dark:text-[#F9F8F8]">
@@ -24,10 +25,17 @@ const Home: NextPage = () => {
             </p>
             <div className="flex justify-start items-center gap-5">
               <button className="btn btn-secondary px-6 text-lg font-normal">
-                Keep Exploring
+                Keep Exploring{' '}
+                <ArrowSvg width="17px" height="17px" fill="#fff" />
               </button>
               <button className="btn btn-primary px-6 text-lg font-normal">
-                Get Started
+                Get Started{' '}
+                <ArrowSvg
+                  width="17px"
+                  height="17px"
+                  fill="#fff"
+                  className={styles.heroButton}
+                />
               </button>
             </div>
           </div>
