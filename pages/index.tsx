19b9555a7ed5a6328cfb18a06d987598346ffd13
Nikhil Sharma <nikhilsharmamusic2000@gmail.com>
2022-06-16 20:57:30 +0530

    fixed padding on small screen

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ const Home: NextPage = () => {
 
       <main className="overflow-hidden py-5 ">
         <div className="flex w-full h-screen max-h-[85vh] relative">
-          <div className="w-full xl:w-1/2 lg:w-7/12 md:w-9/12 sm:w-full flex flex-col justify-center p-20">
+          <div className="w-full xl:w-1/2 lg:w-7/12 md:w-9/12 sm:w-full flex flex-col justify-center p-10 sm:p-20">
             <h1 className="font-bold text-2xl sm:text-left text-[#3F3D56] dark:text-[#E4E4E4] mb-8">
               What is Hydra ?
             </h1>
