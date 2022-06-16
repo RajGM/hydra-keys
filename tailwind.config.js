08fe7b02927c3729fcc88f33313b77741a4d7e6c
Nikhil Sharma <nikhilsharmamusic2000@gmail.com>
2022-06-16 20:45:49 +0530

    added responsiveness

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        nunito: ['Nunito', "sans-serif"]
+        nunito: ['Nunito', 'sans-serif'],
       },
     },
   },
@@ -16,23 +16,23 @@ module.exports = {
     themes: [
       {
         light: {
-          ...require("daisyui/src/colors/themes")["[data-theme=light]"],
-          primary: "#2F2E41",
-          secondary: "#5F5AB4",
-          "base-100": "#F0F0F0",
-          "primary-content": "#FFFFFF",
-          "--btn-text-case": "capitalize",
+          ...require('daisyui/src/colors/themes')['[data-theme=light]'],
+          primary: '#2F2E41',
+          secondary: '#5F5AB4',
+          'base-100': '#F0F0F0',
+          'primary-content': '#FFFFFF',
+          '--btn-text-case': 'capitalize',
         },
       },
       {
         dark: {
-          ...require("daisyui/src/colors/themes")["[data-theme=dark]"],
-          primary: "#C27501",
-          secondary: "#5F5AB4",
-          "base-100": "#3F3D56",
-          "primary-content": "#F1F1F1",
-          "base-content": "#E6E6E6",
-          "--btn-text-case": "capitalize",
+          ...require('daisyui/src/colors/themes')['[data-theme=dark]'],
+          primary: '#C27501',
+          secondary: '#5F5AB4',
+          'base-100': '#3F3D56',
+          'primary-content': '#F1F1F1',
+          'base-content': '#E6E6E6',
+          '--btn-text-case': 'capitalize',
         },
       },
     ],
