1f9e914f6b073f93d98a9fada0d6035116d53d70
unknown <alex.delaitre123@gmail.com>
2022-06-20 13:52:09 +0100

    added authority + users in a wallet and return wallet user is in instead of user details for /api/wallet/user/<pubkey>

--- pages/api/createHydraWallet/index.ts
+++ pages/api/createHydraWallet/index.ts
@@ -3,13 +3,13 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import { Membership, PrismaClient } from '@prisma/client';
 import { MembershipModel } from '@glasseaters/hydra-sdk';
 
-const prisma=new PrismaClient();
+const prisma = new PrismaClient();
 
 export default async function handler(req, res) {
   if (req.method === 'POST') {
     console.log("post");
-    const body=req.body;
-    try{
+    const body = req.body;
+    try {
       //Body for new wallet should be as follows:
       //wallet pubkey
       //authority
@@ -17,25 +17,26 @@ export default async function handler(req, res) {
       //totalShares (can later update this to be automatic though)
       const initMembers: Membership[] = [];
       const savedWallet = await prisma.wallet.create({
-        data:{
-          pubkey:body.pubkey,
-          authority:body.authority,
-          acceptSPL:body.acceptSPL,
-          memberShipType:body.memberShipType,
-          totalShares:body.totalShares
+        data: {
+          name: body.name,
+          pubkey: body.pubkey,
+          authority: body.authority,
+          acceptSPL: body.acceptSPL,
+          memberShipType: body.memberShipType,
+          totalShares: body.totalShares
 
         }
       });
       console.log(savedWallet);
       console.log(body);
     }
-    catch{
+    catch {
       console.log("error");
       return;
     }
     return;
   } else {
-    res.status(200).json({found:false});
+    res.status(200).json({ found: false });
     return;
   }
 
--- pages/api/wallets/user/[viewUserPubkey].ts
+++ pages/api/wallets/user/[viewUserPubkey].ts
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { Membership, PrismaClient } from '@prisma/client';
+import { Membership, PrismaClient,Wallet } from '@prisma/client';
 import { MembershipModel } from '@glasseaters/hydra-sdk';
 
 const prisma=new PrismaClient();
@@ -15,16 +15,19 @@ export default async function handler(
     const wallets=await prisma.wallet.findMany();
     const membersdb=await prisma.membership.findMany();
     console.log(viewUserPubkey);
-    const result: []  = await prisma.$queryRaw`SELECT * FROM membership WHERE memberPubkey=${viewUserPubkey}`;
-    console.log(await prisma.$queryRaw`SELECT * FROM membership WHERE memberPubkey=${viewUserPubkey}`);
+    console.log(wallets);
+
+    console.log(await prisma.$queryRaw`SELECT * FROM wallet`);
+    const result: []  = await prisma.$queryRaw`SELECT * FROM wallet WHERE pubkey=( SELECT walletPubkey FROM membership WHERE membership.memberPubkey=${viewUserPubkey}) 
+    UNION SELECT * FROM wallet WHERE authority=${viewUserPubkey}`;
+    //const authorityOf: [] = await prisma.$queryRaw`SELECT * FROM wallet WHERE authority=${viewUserPubkey}`;
+    //console.log(await prisma.$queryRaw`SELECT * FROM membership WHERE memberPubkey=${viewUserPubkey}`);
     console.log("result");
     console.log(result);
     if(result.length>0){
         res.status(200).json({found:true, 
-            sharesOf:result});
+            in:result});
             return;
     }
-    
-    res.status(200).json({found:false});
-    
+      res.status(200).json({found:false});
   }
