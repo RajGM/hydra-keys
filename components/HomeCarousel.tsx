d0e480cb0548022b2de98cec348479da7bfe68a6
Anshuman Singh <80671124+anshuman-dev@users.noreply.github.com>
2022-06-24 18:54:13 +0530

    Update HomeCarousel.tsx
    All icons on screen at once

--- components/HomeCarousel.tsx
+++ components/HomeCarousel.tsx
@@ -38,11 +38,47 @@ const HomeCarousel = () => {
               <div className="slides-holder__item">
                 <img src="/grow.png" alt="img" />
               </div>
+              <div className="slides-holder__item">
+                <img src="/NFT.png" alt="img" />
+              </div>
+              <div className="slides-holder__item">
+                <img src="/wallet.png" alt="img" />
+              </div>
+              <div className="slides-holder__item">
+                <img src="/grow.png" alt="img" />
+              </div>
             </div>
             <div className="descriptions">
               <div className="descriptions__item descriptions__item_visible">
                 <h1>NFT Membership</h1>
                 <p className="description">
+                  Using NFTs, the membership is tied to an NFT mint address
+                  instead of static public address. Each NFT mint address can
+                  still have a different quantity of shares as in the Wallet
+                  model
+                </p>
+              </div>
+              <div className="descriptions__item">
+                <h1>Wallet Membership</h1>
+                <p className="description">
+                  {' '}
+                  One of the simplest membership model. It's just a list of each
+                  Member's public address and the number of shares they own.
+                </p>
+              </div>
+              <div className="descriptions__item">
+                <h1>Token Membership</h1>
+                <p className="description">
+                  {' '}
+                  The Token is the most flexible membership model, but is a bit
+                  more complicated. In this model, Membership is associated with
+                  staked ownership of the specified Token.
+                </p>
+              </div>
+              <div className="descriptions__item">
+                <h1>NFT Membership</h1>
+                <p className="description">
+                  {' '}
                   Using NFTs, the membership is tied to an NFT mint address
                   instead of static public address. Each NFT mint address can
                   still have a different quantity of shares as in the Wallet
