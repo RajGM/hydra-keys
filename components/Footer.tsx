3f581ad5be22bd911b0d5e883a94500f0cdaef47
RajGM <rajgmsocial19@gmail.com>
2022-06-25 22:18:27 +0530

    Dev:Footer complete according to UI design with mobile responsiveness
    Signed-off-by: RajGM <rajgmsocial19@gmail.com>

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,15 +3,18 @@ import type { NextPage } from 'next'
 const Footer: NextPage = () => {
 
     return (
-        <footer className="footer p-20 bg-neutral text-neutral-content grid-flow-row footerDiv" >
+        <footer className="footer p-20 bg-neutral text-neutral-content grid-flow-row footerDiv" style={{color:"white"}}>
             <div className="items-center grid-flow-col">
-                <div  className='footerLogoDiv'>
-                    <img id="logoImg" src="logo_modified.svg" width="80" height="78" />
+                <div  className='footerLogoDiv '>
+                    <img id="logoImg" src="logo.png" width="80" height="78" />
                 </div>
-                <div style={{ width: "56px" }}></div>
+                <div style={{ width: "25px" }}></div>
                 <div >
                     <p>Hydra wallet</p>
                 </div>
+            </div>
+            <div>
+                
             </div>
             <div >
                 <div>
