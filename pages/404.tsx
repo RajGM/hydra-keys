87c072997d6dbe5a34c1b4871da521b5fdc05c94
Nikhil Sharma <nikhilsharmamusic2000@gmail.com>
2022-06-27 15:46:54 +0530

    added button to homepage

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from 'next'
 import Image from 'next/image'
+import Link from 'next/link'
 
 const FourOhFour: NextPage = () => {
   return (
@@ -14,9 +15,14 @@ const FourOhFour: NextPage = () => {
       <h1 className="w-full text-center md:text-center text-6xl sm:text-6xl md:text-7xl lg:text-8xl font-bold text-primary dark:text-white">
         404
       </h1>
-      <p className="w-full text-center md:text-center text-xl md:text-2xl font-bold text-primary p-1 px-2 dark:text-white">
+      <p className="w-full text-center md:text-center text-xl md:text-2xl font-bold text-primary p-1 px-5 dark:text-white">
         Hey Captain! Looks like you're heading to a wrong planet!
       </p>
+      <Link href="/" passHref>
+        <button className="btn w-fit btn-secondary px-6 text-lg font-normal">
+          Back to homepage
+        </button>
+      </Link>
     </div>
   )
 }
