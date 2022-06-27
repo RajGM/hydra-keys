03b415bfb8b71484546a041b47eb91f494d36224
Nikhil Sharma <nikhilsharmamusic2000@gmail.com>
2022-06-27 15:42:07 +0530

    added text

--- pages/404.tsx
+++ pages/404.tsx
@@ -3,11 +3,20 @@ import Image from 'next/image'
 
 const FourOhFour: NextPage = () => {
   return (
-    <div className="container mx-auto gap-10 flex flex-col justify-center items-center my-10">
-      <Image src="/404.png" layout="fixed" width="150px" height="150px" />
-      <h1 className="w-full text-center md:text-center text-3xl md:text-4xl font-bold text-primary dark:text-white">
-        404 | Page not found
+    <div className="container mx-auto gap-10 flex flex-col justify-center items-center my-0 min-h-[70%]">
+      <Image
+        src="/404.png"
+        layout="fixed"
+        width="150px"
+        height="150px"
+        className="shadow-2xl"
+      />
+      <h1 className="w-full text-center md:text-center text-6xl sm:text-6xl md:text-7xl lg:text-8xl font-bold text-primary dark:text-white">
+        404
       </h1>
+      <p className="w-full text-center md:text-center text-xl md:text-2xl font-bold text-primary p-1 px-2 dark:text-white">
+        Hey Captain! Looks like you're heading to a wrong planet!
+      </p>
     </div>
   )
 }
