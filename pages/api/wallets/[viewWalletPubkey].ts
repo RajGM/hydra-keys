89859eb6f947616455dfc4fb1a9b5f2766cff709
alexandredelaitre <alex.delaitre123@gmail.com>
2022-06-27 18:27:55 +0100

    added cluster queries

--- pages/api/wallets/[viewWalletPubkey].ts
+++ pages/api/wallets/[viewWalletPubkey].ts
@@ -11,19 +11,22 @@ export default async function handler(
     res: NextApiResponse
   ) {
     console.log(req.query);
-    const { viewWalletPubkey } = req.query; 
+    let { cluster, viewWalletPubkey } = req.query; 
+    if(cluster==undefined){
+        cluster="mainnet-beta";
+    }
     console.log(viewWalletPubkey);
     const wallets=await prisma.wallet.findMany();
     const membersdb: Membership[] =await prisma.membership.findMany();
     console.log(wallets);
     console.log(membersdb);
-    const result = await prisma.$queryRaw`SELECT * FROM membership WHERE walletPubkey=${viewWalletPubkey}`;
+    const result = await prisma.$queryRaw`SELECT * FROM membership WHERE walletPubkey=${viewWalletPubkey} AND cluster=${cluster}`;
     console.log("result");
     console.log(result);
     for (let index = 0; index < wallets.length; index++) {
         const element = wallets[index];
         console.log(element.pubkey); //I can do this since wallet addresses must be unique and there will be no two wallets with the same address
-        if(element.pubkey===viewWalletPubkey){
+        if(element.pubkey===viewWalletPubkey && element.cluster==cluster){
             res.status(200).json({found:true, 
                 wallet:element,members:result});
 
